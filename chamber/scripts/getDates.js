404c159324efd418724d3ae6d1d85e55c2b17a51
adamgsheridan <adamgsheridan7@gmail.com>
2023-10-21 23:14:43 -0600

    big update

---
+++ chamber/scripts/getDates.js
@@ -0,0 +1,6 @@
+function displayLastModified() {
+    let lastModified = new Date(document.lastModified);
+    document.querySelector('#lastModified').innerHTML = lastModified
+}
+
+displayLastModified();
