0fe1ebb006974e1532c0a2aff83ad94edf4777e5
adamgsheridan <adamgsheridan7@gmail.com>
2024-07-03 00:47:25 -0600

    chamber site update

--- chamber/scripts/hamburgerMenu.js
+++ chamber/scripts/hamburgerMenu.js
@@ -1,7 +1,5 @@
 function toggleMenu(){
-    console.log("Hello")
     const nav = document.getElementsByTagName("nav")[0]
-    console.log(nav)
     if (nav.className == '')
     {
         nav.className = 'hide'
---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,14 @@
+function calculateWindchill() {
+    let airTemp = 50;
+    let windspeed = 3;
+    let windchill = 35.74 + .6215 * airTemp - 35.75 * windspeed **.16 + .4275 * airTemp * windspeed **.16;
+
+    if(airTemp < 50 || windspeed < 3) {
+        document.getElementById('windchill').innerHTML = "N/A"
+    }
+    else {
+        document.getElementById('windchill').innerHTML = "Windchill: " + windchill;
+    }
+}
+
+calculateWindchill();
