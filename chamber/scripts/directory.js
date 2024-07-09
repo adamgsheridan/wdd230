c2f26e1c9c6c862ff95e8685d78fd9123cd874d0
adamgsheridan <adamgsheridan7@gmail.com>
2024-07-09 01:32:18 -0600

    color update

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -6,24 +6,42 @@ selectGrid.addEventListener("click", ()=>{
     if (!selectGrid.classList.contains("active")){
         selectGrid.classList.add("active")
         selectList.classList.remove("active")
-        selectData.classList.add("directory-cards")
-        selectData.classList.remove("directory-list")
+        selectData.classList.add("light-directory-cards")
+        selectData.classList.remove("light-directory-list")
     }
 })
-6
+
+selectGrid.addEventListener("click", ()=>{
+    if (!selectGrid.classList.contains("active")){
+        selectGrid.classList.add("active")
+        selectList.classList.remove("active")
+        selectData.classList.add("dark-directory-cards")
+        selectData.classList.remove("dark-directory-list")
+    }
+})
+
 selectList.addEventListener("click", ()=>{
     if (!selectList.classList.contains("active")){
         selectList.classList.add("active")
         selectGrid.classList.remove("active")
-        selectData.classList.add("directory-list")
-        selectData.classList.remove("directory-cards")
+        selectData.classList.add("light-directory-list")
+        selectData.classList.remove("light-directory-cards")
+    }
+})
+
+selectList.addEventListener("click", ()=>{
+    if (!selectList.classList.contains("active")){
+        selectList.classList.add("active")
+        selectGrid.classList.remove("active")
+        selectData.classList.add("dark-directory-list")
+        selectData.classList.remove("dark-directory-cards")
     }
 })
 
 const url = "./data/members.json"
 
 const displayBusinesses = (businesses) => {
-    const cards = document.querySelector(".directory-cards")
+    const cards = document.querySelector(".light-directory-cards")
     businesses.forEach((business) => {
         let card = document.createElement("section")
         card.innerHTML = `
@@ -31,7 +49,7 @@ const displayBusinesses = (businesses) => {
         <p>${business.name}</p>
         <p>${business.address}</p>
         <p>${business.phonenumber}</p>
-        <p><a class="card-website" href="${business.websiteurl}">Click Here To Visit Company Website</a></p>
+        <p><a class="light-card-website" href="${business.websiteurl}">Click Here To Visit Company Website</a></p>
         `
         cards.appendChild(card)
     })
@@ -45,8 +63,10 @@ async function getData() {
     }
     else {
         console.error("Something went wrong when loading the business data.")
-        const cards = document.querySelector("directory-cards")
-        cards.innerHTML = "<section><h1>Something went wrong when loading the business data.</h1></section>"
+        const lightcards = document.querySelector("light-directory-cards")
+        const darkcards = document.querySelector("dark-directory-cards")
+        lightcards.innerHTML = "<section><h1>Something went wrong when loading the business data.</h1></section>"
+        darkcards.innerHTML = "<section><h1>Something went wrong when loading the business data.</h1></section>"
     }
 }
 
