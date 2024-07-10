8b244900c51b1e182b90f290a5ac569308bd3809
adamgsheridan <adamgsheridan7@gmail.com>
2024-07-09 19:44:30 -0600

    banner 2.0

--- chamber/scripts/meetAndGreet.js
+++ chamber/scripts/meetAndGreet.js
@@ -1,11 +1,19 @@
 let fday = new Date().getDay();
 
 if (fday >= 1 && fday <= 4) {
-    document.querySelector(".meet-greet").classList.add("show")
+    document.querySelector(".light-meet-greet").classList.add("show")
 }
 
-document.querySelector(".meet-greet button").addEventListener("click", ()=> {
-    document.querySelector(".meet-greet").classList.remove("show")
+document.querySelector(".light-meet-greet button").addEventListener("click", ()=> {
+    document.querySelector(".light-meet-greet").classList.remove("show")
+})
+
+if (fday >= 1 && fday <= 4) {
+    document.querySelector(".dark-meet-greet").classList.add("show")
+}
+
+document.querySelector(".dark-meet-greet button").addEventListener("click", ()=> {
+    document.querySelector(".dark-meet-greet").classList.remove("show")
 })
 
 function deferToJoin() {
